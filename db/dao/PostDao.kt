9b73077381f92b76282986a23b174c6aa223839b
Nayan <ndhabarde11@gmail.com>
2019-05-21 23:14:06 +0530

    Changed groupId to userId

--- db/dao/PostDao.kt
+++ db/dao/PostDao.kt
@@ -13,7 +13,7 @@ interface PostDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(post: Post)
 
-    @Query("SELECT * FROM Post WHERE groupId = :groupId ORDER BY createdAt DESC")
+    @Query("SELECT * FROM Post WHERE userId = :userId ORDER BY createdAt DESC")
     fun allPosts(userId: String): DataSource.Factory<Int, Post>
 
     @Query("DELETE FROM Post")
